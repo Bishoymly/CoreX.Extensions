e96addae969b24b372e927be34e2e83ee35e2845
Bishoy Labib <Bishoy.Labib@integrant.com>
2019-08-11 22:46:25 +0200

    Added swagger documentation from

--- src/MicroserviceTemplate/Controllers/ValuesController.cs
+++ src/MicroserviceTemplate/Controllers/ValuesController.cs
@@ -6,37 +6,59 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MicroserviceTemplate.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
+        /// <summary>
+        /// Gets all value items
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        /// <summary>
+        /// Gets one value item
+        /// </summary>
+        /// <param name="id">The required item id</param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        /// <summary>
+        /// Creates a new value item
+        /// </summary>
+        /// <param name="value">value to be created</param>
+        /// <response code="200">If the item is created successfully</response>
+        /// <response code="400">If the item is null</response>   
         [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        /// <summary>
+        /// Updates an item
+        /// </summary>
+        /// <param name="id">The item key to update</param>
+        /// <param name="value">The new value item</param>
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        /// <summary>
+        /// Deletes an item
+        /// </summary>
+        /// <param name="id">The item key to delete</param>
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
